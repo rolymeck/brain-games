0990a3b2a3e6d3e353d2a756339d7bd7e39de7a2
root <root@DESKTOP-NDSEH5I.localdomain>
2019-07-10 21:38:56 +0400

    update index.js -> evengame

--- src/index.js
+++ src/index.js
@@ -8,24 +8,22 @@ export const welcomeUser = () => {
 };
 
 export const evenGame = (userName) => {
-  let attempt = 1;
-  const gameRound = () => {
+  const gameRound = (round = 1) => {
+    if (round > 3) {
+      console.log(`Congratulations, ${userName}!`);
+      return true;
+    }
     const number = Math.round(Math.random() * 100);
     const isEven = number % 2 === 0;
     const correctAnswer = isEven ? 'yes' : 'no';
     console.log(`Question: ${number}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer && attempt === 3) {
-      console.log(`Correct!\nCongratulations, ${userName}!`);
-      return 'win';
-    }
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      attempt += 1;
-      return gameRound();
+      return gameRound(round + 1);
     }
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    return 'lose';
+    return false;
   };
   return gameRound();
 };
